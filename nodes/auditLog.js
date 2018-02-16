3fd25d7a4842363fbb77f748061003d2ccb9f0c8
Richard Cartwright <spark@streampunk.media>
2018-02-16 14:19:10 +0000

    Adding audit log node.

---
+++ nodes/auditLog.js
@@ -0,0 +1,88 @@
+/* Copyright 2018 Streampunk Media Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+const os = require('os');
+
+module.exports = function (RED) {
+  var log = [];
+  var seq = 0;
+  const hostname = os.hostname();
+  const networkIfs = (nis =>
+    [].concat.apply([], Object.keys(nis).map(k => nis[k]))
+      .filter(i => !i.internal)
+      .map(i => i.address)
+  )(os.networkInterfaces());
+
+  function AuditLog (config) {
+    RED.nodes.createNode(this, config);
+
+    this.on('input', msg => {
+      log.push(msg);
+      if (log.length > config.limit && log.length % 10 === 0) {
+        log = log.slice(-1000);
+      }
+      msg.timestamp = Date.now();
+      msg.sequence = seq++;
+      msg.profile = config.profile;
+      msg.hostname = hostname;
+      msg.networkInterfaces = networkIfs;
+      msg.auditLogName = config.name;
+
+      // Work out where the message came from
+      if (msg.req.method) {
+        msg.type = `HTTP ${msg.req.method}`;
+        msg.version = msg.req.params.ver ? msg.req.params.ver : 'unknown';
+        msg.api = msg.req.params.api ? msg.req.params.api : 'unknown';
+
+        if (msg.req.method === 'POST' || msg.req.method === 'PUT') {
+          if (msg.validated === undefined) {
+            msg.validated = false;
+          }
+        }
+      }
+
+      if (config.console || config.debug) {
+        let debugMsg = {
+          timestamp : msg.timestamp,
+          sequence: msg.sequence,
+          type: msg.type,
+          version: msg.version,
+          api: msg.api,
+          payloadLength: msg.payload.length,
+          validated: msg.validated,
+          valud: msg.valid,
+          payload: msg.payload.slice(0, 60) +
+            msg.payload.length > 60 ? ' ...' : ''
+        };
+        if (config.debug) {
+          RED.comms.publish('debug', { msg: debugMsg });
+        }
+        if (config.console) {
+          this.log(JSON.stringify(debugMsg, null, 2));
+        }
+      }
+
+      this.send(msg);
+    });
+
+    this.on('close', this.close);
+  }
+  
+  AuditLog.prototype.getLog = function () {
+    return log.slice(0);
+  };
+
+  RED.nodes.registerType('audit-log', AuditLog);
+};
