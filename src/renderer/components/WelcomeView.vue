2b0d49ce9d567263c382afe2f74ed0f83ec145a8
Simon Rogers <scriptorian@streampunk.media>
2018-03-30 17:16:44 +0200

    Remove links on welcome page

--- src/renderer/components/WelcomeView.vue
+++ src/renderer/components/WelcomeView.vue
@@ -15,19 +15,16 @@
             </v-layout>
           </v-container>
         </v-card-media>
-        <v-card-title>
-          <div>
-            <span class="subheading">
-              For more information on ZeNMOS, check out the <a href="https://github.com/Streampunk/zenmos#README.md" target="_blank">documentation</a>. Find a bug? Report it on the github <a href="https://github.com/Streampunk/zenmos/issues" target="_blank" title="contribute">issue board</a>.
-            </span>
-          </div>
-        </v-card-title>
         <v-divider></v-divider>
         <v-card-actions class="pt-3 pb-3">
           <v-spacer></v-spacer>
-          <v-btn class="link-btn" @click="open('https://github.com/AMWA-TV/nmos-discovery-registration')">AMWA IS-04</v-btn>
+          <v-btn class="link-btn" color="primary" @click="open('https://github.com/Streampunk/zenmos#README.md')">ZeNMOS
+            <v-icon dark right>launch</v-icon>
+          </v-btn>
           <v-spacer></v-spacer>
-          <v-btn class="link-btn" @click="open('https://github.com/Streampunk/zenmos#README.md')">ZeNMOS</v-btn>
+          <v-btn class="link-btn" color="primary" @click="open('https://github.com/AMWA-TV/nmos-discovery-registration')">AMWA IS-04
+            <v-icon dark right>launch</v-icon>
+          </v-btn>
           <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
@@ -47,18 +44,6 @@
 </script>
 
 <style scoped>
-  .centered
-  {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .logo
-  {
-    max-width: 100%;
-  }
-
   .link-btn
   {
     width: 150px;
