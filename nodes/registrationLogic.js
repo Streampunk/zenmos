b52e6b16574d7a88ca1858344ba82dcf3cece93f
Richard Cartwright <spark@streampunk.media>
2018-04-08 14:03:31 -0700

    Adding demo files and fixing bug affecting HTTP responses.

--- nodes/registrationLogic.js
+++ nodes/registrationLogic.js
@@ -82,6 +82,9 @@ module.exports = function (RED) {
       if (!msg.type.startsWith('HTTP REQ')) {
         return; // Only process HTTP messages
       }
+      if (typeof msg.validated !== 'boolean') {
+        msg.validated = false;
+      }
       if (msg.validated !== config.validated) {
         return; // Process valid messages or not depending on configuration
       }
